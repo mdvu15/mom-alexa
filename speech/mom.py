467e3c96b22b58a055ae5a60e704857032d2c36f
Minh Vu <mdvu15@earlham.edu>
2017-01-15 11:36:33 -0600

    money transfer works

--- speech/mom.py
+++ speech/mom.py
@@ -1,4 +1,12 @@
 import json
+from blockcypher import create_wallet_from_address
+from blockcypher import generate_new_address
+from blockcypher import get_address_details
+from blockcypher import simple_spend
+from blockcypher import get_total_balance
+from blockcypher import create_unsigned_tx
+from blockcypher import send_faucet_coins
+
 
 def lambda_handler(event, context):
     #if (event["session"]["application"]["applicationId"] !=
@@ -29,6 +37,8 @@ def on_intent(intent_request, session):
         return get_hungry_status()
     elif intent_name == "HiMom":
         return get_hi_status()
+    elif intent_name == "Money":
+        return get_money()
     elif intent_name == "AMAZON.HelpIntent":
         return get_welcome_response()
     elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
@@ -50,7 +60,7 @@ def handle_session_end_request():
 def get_welcome_response():
     session_attributes = {}
     card_title = "PARENT MODE"
-    speech_output = "Entering mommy mode"
+    speech_output = "Becoming your mom"
     reprompt_text = "Honey, are you there?"
     should_end_session = False
     return build_response(session_attributes, build_speechlet_response_text(
@@ -77,6 +87,25 @@ def get_hi_status():
 
     return build_response(session_attributes, build_speechlet_response_text(
         card_title, speech_output, reprompt_text, should_end_session))
+
+def get_money():
+    try:
+        #print(get_total_balance('C22cpHQAzQ2rt7Jj1fQjevQeTdZhKbUysW', coin_symbol='bcy', api_key='4bcc9dcc6bc449fc85cd525802b30fa2'))
+        simple_spend(from_privkey='356ce8496d761f51aa5939da9f7bbfb94886234f1f8eb5507bea9f3676e6c8e8', to_address='C22cpHQAzQ2rt7Jj1fQjevQeTdZhKbUysW', to_satoshis=500, coin_symbol='bcy', api_key='4bcc9dcc6bc449fc85cd525802b30fa2')
+        session_attributes = {}
+        card_title = ""#"I transferred money to your account. Your balance is now " + str(get_total_balance('C22cpHQAzQ2rt7Jj1fQjevQeTdZhKbUysW', coin_symbol='bcy', api_key='4bcc9dcc6bc449fc85cd525802b30fa2')) + " satoshis."
+        reprompt_text = ""
+        should_end_session = False
+        speech_output = "I transferred money to your account. Your balance is now " + str(get_total_balance('C22cpHQAzQ2rt7Jj1fQjevQeTdZhKbUysW', coin_symbol='bcy', api_key='4bcc9dcc6bc449fc85cd525802b30fa2')) + " satoshis."
+        print speech_output
+        return build_response(session_attributes, build_speechlet_response_text(card_title, speech_output, reprompt_text, should_end_session))
+
+    except:
+        session_attributes = {}
+        carde_title = ""
+        should_end_session = False
+        speech_output = "I have nothing left to give"
+        return build_response(session_attributes, build_speechlet_response_text(card_title, speech_output, reprompt_text, should_end_session))
 
 '''def get_train_times(intent):
     session_attributes = {}
